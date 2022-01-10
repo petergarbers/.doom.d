e3575e77f44e67ddb3a6ca8f209229b1ad2f51e7
petergarbers <peter@devthe.net>
2022-01-10 09:26:07 -0700

    stuff

--- config.el
+++ config.el
@@ -52,6 +52,26 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
+
+
+
+
+
+
+(use-package! cider
+  :after clojure-mode
+  :config
+  (setq cider-ns-refresh-show-log-buffer t
+        cider-show-error-buffer t ;'only-in-repl
+        cider-font-lock-dynamically nil ; use lsp semantic tokens
+        cider-eldoc-display-for-symbol-at-point nil ; use lsp
+        cider-prompt-for-symbol nil)
+  (set-popup-rule! "*cider-test-report*" :side 'right :width 0.4)
+  (set-popup-rule! "^\\*cider-repl" :side 'bottom :quit nil)
+  (set-lookup-handlers! 'cider-mode nil) ; use lsp
+  (add-hook 'cider-mode-hook (lambda () (remove-hook 'completion-at-point-functions #'cider-complete-at-point))) ; use lsp
+  )
+
 (use-package! clj-refactor
   :after clojure-mode
   :config
@@ -69,21 +89,5 @@
   (setq clojure-indent-style 'align-arguments
         clojure-thread-all-but-last t
         smartparens-global-mode t
-        smartparens-strict-mode t
-        sp-use-paredit-bindings t)
+        smartparens-strict-mode t)
   (cljr-add-keybindings-with-prefix "C-c C-c"))
-
-
-(use-package! cider
-  :after clojure-mode
-  :config
-  (setq cider-ns-refresh-show-log-buffer t
-        cider-show-error-buffer t ;'only-in-repl
-        cider-font-lock-dynamically nil ; use lsp semantic tokens
-        cider-eldoc-display-for-symbol-at-point nil ; use lsp
-        cider-prompt-for-symbol nil)
-  (set-popup-rule! "*cider-test-report*" :side 'right :width 0.3)
-  (set-popup-rule! "^\\*cider-repl" :side 'bottom :quit nil)
-  (set-lookup-handlers! 'cider-mode nil) ; use lsp
-  (add-hook 'cider-mode-hook (lambda () (remove-hook 'completion-at-point-functions #'cider-complete-at-point))) ; use lsp
-  )
